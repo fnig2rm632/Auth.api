0f4052bc3024d21cd69f3f4cf8df11bbff0cbeec
fnig2rm632 <fnig2rm632@yandex.ru>
2026-05-17 12:16:56 +0300

    feat(domain): Create castom error for Entities

--- src/Auth.Domain/Common/Error.cs
+++ src/Auth.Domain/Common/Error.cs
@@ -12,6 +12,82 @@ public record Error(string Message, ErrorType ErrorType)
     private static Error Unavailable(string massage = "Service Unavailable") => new(massage, ErrorType.Unavailable);
     private static Error Timeout(string massage = "Gateway Timeout") => new(massage, ErrorType.Timeout);
     
+    public static class Session
+    {
+        public static Error IdCannotBeEmpty => Validation("Session ID cannot be empty");
+        public static Error RefreshTokenHashCannotBeEmpty => Validation("Refresh token hash cannot be empty");
+        public static Error IpAddressCannotBeEmpty => Validation("IpAddress cannot be empty");
+        public static Error InvalidExpirationDate => Validation("Expiration date cannot be in the max or min date");
+        public static Error InvalidCreationDate => Validation("Creation date cannot be in the max or min date");
+        public static Error InvalidRevocationDate => Validation("Revocation date cannot be in the max or min date");
+        public static Error ExpiresAtCannotBeInPast => Validation("Expires  at can't be in the past");
+        public static Error CreationDateInFuture => Validation("Creation  date can't be in the future");
+        public static Error CreationDateAfterExpiration => Validation("Creation date can't be after expiration");
+        public static Error RevokedBeforeCreation => Validation("Revoked data cannot be before creation data");
+        public static Error RevocationDateInFuture => Validation("Revocation date can't be in the future");
+        public static Error AlreadyRevoked => Validation("Already revoked session");
+        public static Error Revoked => Validation("Session revoked ");
+    }
+    
+    public static class User
+    {
+        public static Error IdCannotBeEmpty => Validation("User ID cannot be empty");
+        public static Error LoginCannotBeNull => Validation("Login user value cannot be empty");
+        public static Error EmailCannotBeNull => Validation("Email user value cannot be empty");
+        public static Error PasswordHashCannotBeNull => Validation("Password hash user value cannot be empty");
+        public static Error CreatedAtInFuture => Validation("User created at in the future");
+        public static Error NewPasswordMustBeDifferent => Conflict("New password must be different");
+    }
+    
+    public static class Code
+    {
+        public static Error CreatedAtInFuture => Validation("Code created at in the future");
+        public static Error ValueCannotBeEmpty => Validation("Code code value cannot be empty");
+        public static Error AlreadyRevoked => Validation("Already revoked session");
+        public static Error InvalidHashEntry => Validation("Hash Code value cannot be empty");
+        public static Error ExpiredOrMissing => NotFound("Verification code not found or expired");
+        public static Error InvalidCode => Validation("Invalid verification code");
+        public static Error ReasonMismatch => Validation("Code was not requested for this action");
+    }
+    
+    public static class Device
+    {   
+        public static Error IdCannotBeEmpty => Validation("Device ID cannot be empty");
+    }
+    
+    public static class Platform
+    {   
+        public static Error IdCannotBeNegative => Validation("Platform ID cannot be negative");
+        public static Error NameCannotBeEmpty => Validation("Name cannot be empty");
+        public static Error NameTooLong => Validation("Platform name cannot be more than 255 characters");
+
+    }
+
+    public static class Provider
+    {
+        public static Error IdCannotBeNegative => Validation("Provider ID cannot be negative");
+        public static Error NameCannotBeEmpty => Validation("Provider name cannot be empty");
+        public static Error NameTooLong => Validation("Provider name cannot be more than 255 characters");
+
+    }
+
+    public static class OAuthToken
+    {
+        public static Error IdCannotBeEmpty => Validation("ID OAuth token cannot be empty");
+        public static Error AccessTokenCannotBeEmpty => Validation("Access token cannot be empty");
+        public static Error RefreshTokenCannotBeEmpty => Validation("Refresh token cannot be empty");
+        public static Error InvalidExpirationDate => Validation("Expiration date cannot be in the max or min date");
+        public static Error ExpiresAtCannotBeInPast => Validation("Expires at can't be in the past");
+    }
+    
+    public static class UserIdentity
+    {
+        public static Error IdCannotBeEmpty => Validation("User ID cannot be empty");
+        public static Error ProviderUserIdCannotBeEmpty => Validation("Provider user ID cannot be empty");
+        public static Error ProviderUserIdTooLong => Validation("Provider user ID cannot be more than 255 characters");
+        public static Error CreatedAtInFuture => Validation("Created time at in the future");
+    }
+    
     public static class Email
     {
         public static Error ValueCannotBeEmpty => Validation("Email value cannot be empty");
