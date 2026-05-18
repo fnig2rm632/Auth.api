0d540a770698dd80c2969693b65f31042d743f0a
fnig2rm632 <fnig2rm632@yandex.ru>
2026-05-18 16:36:49 +0300

    feat(Infrastructure): registrating DatabaseContext in DI

--- src/Auth.Infrastructure/DependencyInjection.cs
+++ src/Auth.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Auth.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Auth.Infrastructure;
@@ -7,6 +9,22 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureDi(this IServiceCollection services, IConfiguration configuration)
     {
+        // Registration db context
+        var hostDb = configuration["ConnectionPostgres:Host"];;
+        var portDb = configuration["ConnectionPostgres:Port"];;
+        var databaseDb = configuration["ConnectionPostgres:Database"];;
+        var usernameDb = configuration["ConnectionPostgres:Username"];;
+        var passwordDb = configuration["ConnectionPostgres:Password"];;
+        
+        var connectionString = 
+            $"Host={hostDb};" +
+            $"Port={portDb};" +
+            $"Database={databaseDb};" +
+            $"Username={usernameDb};" +
+            $"Password={passwordDb}";
+        
+        services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connectionString));
+        
         return services;
     }
 }
