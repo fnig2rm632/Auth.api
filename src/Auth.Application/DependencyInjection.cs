d012202c03e4ff7792b65b1d19ef2ea25dae5119
fnig2rm632 <fnig2rm632@yandex.ru>
2026-05-19 20:39:45 +0300

    fix: Depentency recursion

--- src/Auth.Application/DependencyInjection.cs
+++ src/Auth.Application/DependencyInjection.cs
@@ -1,5 +1,4 @@
 ﻿using Auth.Domain;
-using Auth.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,9 +8,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationDi(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDomainDi(configuration);
-        services.AddInfrastructureDi(configuration);
-        
         return services;
     }
 }
