289614e3852f23fea5c579d3de3d30019dc9477e
fnig2rm632 <fnig2rm632@yandex.ru>
2026-05-22 22:07:40 +0300

    feat(application): create interface ITokenProvider

---
+++ src/Auth.Application/Interfaces/Service/ITokenProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using Auth.Domain.Common;
+using Auth.Domain.Entities;
+
+namespace Auth.Application.Interfaces.Service;
+
+public interface ITokenProvider
+{
+    Result<string> CreateAccessToken(User user);
+    Result<string> CreateRefreshToken(User user);
+    Task<Result<ClaimsPrincipal>> ValidateTokenAsync(string token);
+}
