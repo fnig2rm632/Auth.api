c9328258d54f6f15d0a71497e02438f9ec32fa03
fnig2rm632 <fnig2rm632@yandex.ru>
2026-05-22 22:06:51 +0300

    feat(application): create interface IPasswordHasher

---
+++ src/Auth.Application/Interfaces/Service/IPasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using Auth.Domain.Common;
+using Auth.Domain.ValueObjects;
+
+namespace Auth.Application.Interfaces.Service;
+
+public interface IPasswordHasher
+{
+    Task<Result<string>> HashAsync(Password password);
+    Task<Result<bool>> VerifyAsync(Password password, string passwordHash);
+}
